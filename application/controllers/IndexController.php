8bbbb88aa009eedfd433615c79851bf06badbcac
Nicolas Limage <nlimage@online.net>
2015-05-07 10:49:20 +0200

    added connect_limit parameter to limit graph auto-connection

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,10 +7,11 @@ class Graphite_IndexController extends ActionController
     public function indexAction()
     {
         $this->view->url = sprintf(
-            '%s&target=%s.%s&source=0&width=800&height=700&colorList=049BAF&lineMode=connected',
+            '%s&target=%s.%s&source=0&width=800&height=700&colorList=049BAF&lineMode=connected&connectedLimit=%s',
             $this->getParam('base_url'),
-            $this->getParam('metric_prefix'),
-            $this->getParam('target')
+            $this->getParam('metric_prefix', 'icinga'),
+            $this->getParam('target'),
+            $this->getParam('connect_limit', 10)
         );
     }
 }
